4b22d93619e6487d02ee82b168fb88d1d270daf8
stephen <u6269649@anu.edu.au>
2024-06-10 00:15:15 +1000

    BUILD: event messages modelling for message protocol V2

---
+++ models/event_messages.go
@@ -0,0 +1,43 @@
+package models
+
+import "time"
+
+type BaseEvent struct {
+	Event     string    `json:"event"`
+	UUID      string    `json:"uuid"`
+	Timestamp time.Time `json:"timestamp"`
+	Type      string    `json:"type"`
+	Hostname  string    `json:"hostname"`
+}
+
+type TaskReceivedEvent struct {
+	BaseEvent
+	Args   []interface{}          `json:"args"`
+	Kwargs map[string]interface{} `json:"kwargs"`
+}
+
+type TaskStartedEvent struct {
+	BaseEvent
+}
+
+type TaskSucceededEvent struct {
+	BaseEvent
+	Result interface{} `json:"result"`
+}
+
+type TaskFailedEvent struct {
+	BaseEvent
+	Exception string `json:"exception"`
+	Traceback string `json:"traceback"`
+}
+
+type TaskRevokedEvent struct {
+	BaseEvent
+}
+
+type TaskRetryEvent struct {
+	BaseEvent
+	Exception string    `json:"exception"`
+	Traceback string    `json:"traceback"`
+	ETA       time.Time `json:"eta"`
+}
