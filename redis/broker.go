7b1a8b76d7ab9c2f92dcfb0cca6957b681bbb532
stephen <u6269649@anu.edu.au>
2024-06-10 01:09:53 +1000

    ENH: new logics implemented in redis broker

--- redis/broker.go
+++ redis/broker.go
@@ -5,76 +5,114 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/cloud-mill/go-celery/logger"
 	"time"
 
 	"github.com/cloud-mill/go-celery/models"
 	"github.com/go-redis/redis/v8"
+	"go.uber.org/zap"
 )
 
 type CeleryRedisBroker struct {
-	RedisClient *redis.Client
-	QueueName   string
+	RedisClient    *redis.Client
+	QueueName      string
+	EventQueueName string
 }
 
-func NewRedisCeleryRedisBroker(redisClient *redis.Client) *CeleryRedisBroker {
+func NewCeleryRedisBroker(redisClient *redis.Client) *CeleryRedisBroker {
 	return &CeleryRedisBroker{
-		RedisClient: redisClient,
-		QueueName:   "go-celery",
+		RedisClient:    redisClient,
+		QueueName:      "go-celery",
+		EventQueueName: "go-celery-events",
 	}
 }
 
-func (celeryRedisBroker *CeleryRedisBroker) SendCeleryMessage(
+func (broker *CeleryRedisBroker) SendTaskMessage(
 	ctx context.Context,
-	message models.CeleryMessage,
+	taskMessage models.TaskMessage,
 ) error {
-	jsonBytes, err := json.Marshal(message)
+	jsonBytes, err := json.Marshal(taskMessage)
 	if err != nil {
+		logger.Logger.Error("failed to marshal task message", zap.Error(err))
 		return err
 	}
 
-	_, err = celeryRedisBroker.RedisClient.LPush(ctx, celeryRedisBroker.QueueName, jsonBytes).
-		Result()
+	_, err = broker.RedisClient.LPush(ctx, broker.QueueName, jsonBytes).Result()
 	if err != nil {
+		logger.Logger.Error("failed to push task message to Redis", zap.Error(err))
 		return err
 	}
 
 	return nil
 }
 
-func (celeryRedisBroker *CeleryRedisBroker) getCeleryMessage(
+func (broker *CeleryRedisBroker) GetTaskMessage(
 	ctx context.Context,
-) (*models.CeleryMessage, error) {
-	// BRPOP command to pop the last(right) message from the list (queue), with a timeout of 1 second
-	res, err := celeryRedisBroker.RedisClient.BRPop(ctx, 1*time.Second, celeryRedisBroker.QueueName).
-		Result()
+) (*models.TaskMessage, error) {
+	res, err := broker.RedisClient.BRPop(ctx, 1*time.Second, broker.QueueName).Result()
 	if err != nil {
 		if errors.Is(err, redis.Nil) {
 			return nil, nil
 		}
+		logger.Logger.Error("failed to pop message from Redis", zap.Error(err))
 		return nil, err
 	}
 
 	if len(res) < 2 {
-		// The first element in res is the queue name, and the second is the message.
-		// If we have less than 2 elements, something went wrong.
-		return nil, fmt.Errorf("received an invalid message format from Redis")
+		err := fmt.Errorf("received an invalid message format from Redis")
+		logger.Logger.Error("invalid message format", zap.Error(err))
+		return nil, err
 	}
 
-	var message models.CeleryMessage
+	var message models.TaskMessage
 	if err := json.Unmarshal([]byte(res[1]), &message); err != nil {
+		logger.Logger.Error("failed to unmarshal message", zap.Error(err))
 		return nil, err
 	}
 
 	return &message, nil
 }
 
-func (celeryRedisBroker *CeleryRedisBroker) GetTaskMessage(
+func (broker *CeleryRedisBroker) SendTaskReceivedEvent(
 	ctx context.Context,
-) (*models.TaskMessage, error) {
-	celeryMessage, err := celeryRedisBroker.getCeleryMessage(ctx)
-	if err != nil || celeryMessage == nil {
-		return nil, err
+	event models.TaskReceivedEvent,
+) error {
+	return broker.sendEventMessage(ctx, event)
+}
+
+func (broker *CeleryRedisBroker) SendTaskStartedEvent(
+	ctx context.Context,
+	event models.TaskStartedEvent,
+) error {
+	return broker.sendEventMessage(ctx, event)
+}
+
+func (broker *CeleryRedisBroker) SendTaskSucceededEvent(
+	ctx context.Context,
+	event models.TaskSucceededEvent,
+) error {
+	return broker.sendEventMessage(ctx, event)
+}
+
+func (broker *CeleryRedisBroker) SendTaskFailedEvent(
+	ctx context.Context,
+	event models.TaskFailedEvent,
+) error {
+	return broker.sendEventMessage(ctx, event)
+}
+
+func (broker *CeleryRedisBroker) sendEventMessage(ctx context.Context, event interface{}) error {
+	jsonBytes, err := json.Marshal(event)
+	if err != nil {
+		logger.Logger.Error("failed to marshal event message", zap.Error(err))
+		return err
 	}
 
-	return celeryMessage.ExtractTaskMessage()
+	_, err = broker.RedisClient.LPush(ctx, broker.EventQueueName, jsonBytes).Result()
+	if err != nil {
+		logger.Logger.Error("failed to push event message to Redis", zap.Error(err))
+		return err
+	}
+
+	return nil
 }
